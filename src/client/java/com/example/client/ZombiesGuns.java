9c296bebb0a0d60cbb6984adf497a7c4a824768b
15026 <1502656934@qq.com>
2026-06-09 17:14:46 +0800

    fix

--- src/client/java/com/example/client/ZombiesGuns.java
+++ src/client/java/com/example/client/ZombiesGuns.java
@@ -10,6 +10,7 @@ public class ZombiesGuns {
     public static final Map<Item, String> ZOMBIES_GUN_ITEM_NAMES = Map.ofEntries(
             Map.entry(Items.WOODEN_HOE, "Pistol"),
             Map.entry(Items.STONE_HOE, "Rifle"),
+            Map.entry(Items.GOLDEN_SHOVEL, "Rainbow Rifle"),
             Map.entry(Items.IRON_HOE, "Shotgun"),
             Map.entry(Items.STONE_SHOVEL, "Rocket Launcher"),
             Map.entry(Items.WOODEN_SHOVEL, "Sniper"),
@@ -17,7 +18,8 @@ public class ZombiesGuns {
             Map.entry(Items.IRON_SHOVEL, "Blow Dart"),
             Map.entry(Items.DIAMOND_HOE, "Zombie Soaker"),
             Map.entry(Items.DIAMOND_PICKAXE, "Zombie Zapper"),
-            Map.entry(Items.DIAMOND_AXE, "The Puncher"),
+//            Map.entry(Items.DIAMOND_AXE, "The Puncher"),
+            Map.entry(Items.FLINT_AND_STEEL, "Double Barrel Shotgun"),
             Map.entry(Items.SHEARS, "Elder Gun"),
             Map.entry(Items.GOLDEN_PICKAXE, "Gold Digger")
     );
